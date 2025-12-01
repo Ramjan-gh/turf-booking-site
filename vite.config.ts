6e28e0e6d45ec950070667ed07acde1ddcdd3d47
ramjan-gh <ramzanhridoy@gmail.com>
2025-12-02 00:20:24 +0600

    today done

--- vite.config.ts
+++ vite.config.ts
@@ -54,7 +54,7 @@
       target: 'esnext',
       outDir: 'build',
     },
-    base: './', // ensures correct paths for assets
+    base: '/', // ensures correct paths for assets
     server: {
       port: 3000,
       open: true,
